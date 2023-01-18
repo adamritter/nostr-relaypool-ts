1eb5d9502aba340c9dbf0667f3f3e5a7b219fa94
adamritter <58403584+adamritter@users.noreply.github.com>
2023-01-18 14:32:24 +0000

    add example

---
+++ examples/simple-nodejs/README.md
@@ -0,0 +1,5 @@
+Start with
+
+```
+npm run start
+```
---
+++ examples/simple-nodejs/index.js
@@ -0,0 +1,20 @@
+let NostrRelayPool = require("nostr-relaypool");
+let RelayPool = NostrRelayPool.RelayPool;
+
+let relayPool = new RelayPool();
+relayPool.subscribe(
+  [
+    {
+      authors: [
+        "32e1827635450ebb3c5a7d12c1f8e7b2b514439ac10a67eef3d9fd9c5c68e245",
+      ],
+      limit: 20,
+    },
+  ],
+  [
+    "wss://relay.damus.io",
+    "wss://nostr.fmt.wiz.biz",
+    "wss://relay.snort.social",
+  ],
+  console.log
+);
