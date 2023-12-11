98ba401cd8a66aad1707d1410efa0f5bb3b8d525
Alex Gleason <alex@alexgleason.me>
2023-12-11 15:43:07 -0600

    Simplify metadata-cache.ts

--- metadata-cache.ts
+++ metadata-cache.ts
@@ -1,20 +1,14 @@
 import type {Event} from "nostr-tools";
 
 export class MetadataCache {
-  data: Map<string, Event>;
   promises: Map<string, Promise<Event>>;
   servers: string[];
   constructor(servers?: string[]) {
-    this.data = new Map();
     this.promises = new Map();
     this.servers = servers || ["https://us.rbr.bio", "https://eu.rbr.bio"];
   }
 
   async get(pubkey: string): Promise<Event> {
-    let value = this.data.get(pubkey);
-    if (value) {
-      return Promise.resolve(value);
-    }
     const promise = this.promises.get(pubkey);
     if (promise) {
       return promise;
@@ -23,11 +17,7 @@ export class MetadataCache {
     for (let server of this.servers) {
       rs.push(fetchMetadata(server, pubkey));
     }
-    const r = firstGoodPromise(rs);
-    r.then((x) => {
-      this.data.set(pubkey, x);
-      this.promises.delete(pubkey);
-    });
+    const r = Promise.any(rs);
     this.promises.set(pubkey, r);
     return r;
   }
@@ -39,20 +29,6 @@ async function fetchJSON(url: string) {
     .catch((e) => {
       throw new Error("error fetching " + url + " " + e);
     });
-}
-
-function firstGoodPromise<T>(promises: Promise<T>[]): Promise<T> {
-  return new Promise((resolve, reject) => {
-    let rejects: any[] = [];
-    promises.forEach((p) => {
-      p.then(resolve).catch((rej) => {
-        rejects.push(rej);
-        if (rejects.length === promises.length) {
-          reject(rejects);
-        }
-      });
-    });
-  });
 }
 
 function fetchMetadata(server: string, pubkey: string) {
